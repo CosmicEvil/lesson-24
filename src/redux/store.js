f25745b75b0ba271a4fe22a8afdb4d21560afc09
Wouter Massie <bitbucket@adsomenoise.com>
2021-12-15 21:34:26 +0100

    heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,7 @@ const middlewares = [logger];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
+  console.log("test")
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
